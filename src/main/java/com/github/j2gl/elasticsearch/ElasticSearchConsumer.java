207682c78fcc5097ddb1c1f1d80f71b5c00ec7cb
Juan J. Garcia <juanjo.garcia@gmail.com>
2021-01-24 22:03:00 +0100

    master: Add consumer logic to push data into ElasticSearch

--- src/main/java/com/github/j2gl/elasticsearch/ElasticSearchConsumer.java
+++ src/main/java/com/github/j2gl/elasticsearch/ElasticSearchConsumer.java
@@ -5,6 +5,11 @@ import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -16,6 +21,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
 
 public class ElasticSearchConsumer {
 
@@ -36,19 +44,47 @@ public class ElasticSearchConsumer {
         return new RestHighLevelClient(builder);
     }
 
+    public static KafkaConsumer<String, String> createConsumer(String topic) {
+        final String bootstrapServers = "127.0.0.1:9092";
+        final String groupId = "kafka-demo-elasticsearch";
+        //final String topic = "twitter_tweets";
+
+        final Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // Create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+        consumer.subscribe(Collections.singletonList(topic));
+        return consumer;
+    }
+
     public static void main(String[] args) throws IOException {
         Logger logger = LoggerFactory.getLogger(ElasticSearchConsumer.class.getName());
         RestHighLevelClient client = createClient();
+        KafkaConsumer<String, String> consumer = createConsumer("twitter_tweets");
 
-        String jsonString = "{ \"foo\": \"bar\"}";
-        IndexRequest indexRequest = new IndexRequest("twitter")
-                .source(jsonString, XContentType.JSON);
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records) {
+                final String jsonString = record.value();
 
-        IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
-        String id = indexResponse.getId();
-        logger.info("ID: {}", id);
+                IndexRequest indexRequest = new IndexRequest("twitter")
+                        .source(jsonString, XContentType.JSON);
 
-        client.close();
+                IndexResponse indexResponse = client.index(indexRequest, RequestOptions.DEFAULT);
+                logger.info("ID: {}", indexResponse.getId());
+                try {
+                    Thread.sleep(1_000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+//        client.close();
     }
 
 
